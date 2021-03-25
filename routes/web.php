58e70012beb5b4b6d2d01d38e48ceeb1b7874882
higashida <cervus--ht7578@ezweb.ne.jp>
2021-03-25 21:39:19 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,15 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function(){
     Route::get('news/create','Admin\Newscontroller@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 //課題4
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 Auth::routes();
 
