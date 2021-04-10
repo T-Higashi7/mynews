6a3046a6f99c523ce976ac3defd043821f1d8ceb
higashida <cervus--ht7578@ezweb.ne.jp>
2021-04-10 15:09:20 +0000

    質問

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Controller;
 use App\profile;
 
 
-
 class ProfileController extends Controller
 {
 
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function(){
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');//追記
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');// 追記
 });
 
 //課題4
