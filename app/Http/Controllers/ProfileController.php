aed6a816e6db5b89a0f20d6f5bc055a07c1e7e30
higashida <cervus--ht7578@ezweb.ne.jp>
2021-04-28 02:07:40 +0000

    課題提出

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,16 +11,9 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         $posts = Profile::all()->sortByDesc('updated_at');
-
-        // if (count($posts) > 0) {
-        //     $headline = $posts->shift();
-        // } else {
-        //     $headline = null;
-        // }
-
         // profiles/index.blade.php ファイルを渡している
-        // また View テンプレートに headline、 posts、という変数を渡している
-        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
+        // また View テンプレートにposts、という変数を渡している
+        return view('profile.index', ['posts' => $posts]);
     }
 
 }
