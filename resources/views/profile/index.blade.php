c08a7bddf5f9a4029da002b0488aa5650d695a8b
higashida <cervus--ht7578@ezweb.ne.jp>
2021-04-29 06:52:53 +0000

    課題提出

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,28 +2,6 @@
 
 @section('content')
     <div class="container">
-        <!--<hr color="#c0c0c0">-->
-        <!--@if (!is_null($headline))-->
-        <!--    <div class="row">-->
-        <!--        <div class="headline col-md-10 mx-auto">-->
-        <!--            <div class="row">-->
-        <!--                <div class="col-md-6">-->
-        <!--                    <div class="caption mx-auto">-->
-        <!--                        <div class="name p-2">-->
-        <!--                            <h1>{{ str_limit($headline->name, 70) }}</h1>-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        <!--                </div>-->
-        <!--                <div class="col-md-6">-->
-        <!--                    <p class="hobby mx-auto">{{ str_limit($headline->introduction, 600) }}</p>-->
-        <!--                </div>-->
-        <!--                <div class="col-md-6">-->
-        <!--                    <p class="introduction mx-auto">{{ str_limit($headline->introduction, 600) }}</p>-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--        </div>-->
-        <!--    </div>-->
-        <!--@endif-->
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
